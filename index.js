35ea23e47177a60a3abd39f983edf97b7494c6af
tamanobi <tamanobi@gmail.com>
2019-02-04 01:18:17 +0900

    refactor

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const router = new Router();
 
 router.get('/', async (ctx, next) => {
   const url = ctx.request.query.u
-  console.log(ctx.request.query)
-
   const status = await crawler(url)
   status.headers.forEach((v) => {
       ctx.set(v.key, v.value)
@@ -17,13 +15,13 @@ router.get('/', async (ctx, next) => {
 });
 
 app
-    .use(router.routes())
-    .use(router.allowedMethods())
-    .use(bodyParser())
-    .listen(process.env.PORT || 3000)
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(bodyParser())
+  .listen(process.env.PORT || 3000)
 
 const puppeteer = require('puppeteer');
-const LAUNCH_OPTION = process.env.DYNO ? { args: ['--no-sandbox', '--disable-setuid-sandbox'] } : { headless: false };
+const LAUNCH_OPTION = process.env.HEADLESS ? { args: ['--no-sandbox', '--disable-setuid-sandbox'] } : { headless: false };
 
 const crawler = async (url) => {
   const browser = await puppeteer.launch(LAUNCH_OPTION);
@@ -31,16 +29,14 @@ const crawler = async (url) => {
   page.setViewport({width: 1200, height: 800});
 
   await page.goto(url, { waitUntil: 'networkidle2' });
-  const newsTitles = await page.evaluate(() => {
-    // const elenemts = document.querySelectorAll('.itemlist .title > a');
-    const elenemts = document.querySelectorAll('title');
-    return [].map.call(elenemts, el => el.innerText);
-  });
   const bufScreenshot = await page.screenshot({ fullPage: true, type: 'png'});
 
   await browser.close();
   return {
     body: bufScreenshot,
-    headers: [{key: 'Content-Type', value: 'image/png'}]
+    headers: [
+      {key: 'Content-Type', value: 'image/png'},
+      {key: 'Content-Disposition', value: 'inline'},
+    ]
   }
 }
